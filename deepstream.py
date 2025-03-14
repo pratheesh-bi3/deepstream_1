ec81c2849cd3a4216ddd803af9658234677e64a1
pratheesh-bi3 <pratheeshraj.prince@bi3technologies.com>
2025-03-14 16:26:28 +0530

    Add files via upload
    my_py_code

---
+++ deepstream.py
@@ -0,0 +1,94 @@
+import sys
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GLib
+import pyds
+
+PGIE_CLASS_ID_VEHICLE = 0
+PGIE_CLASS_ID_PERSON = 1
+
+def osd_sink_pad_buffer_probe(pad, info, u_data):
+    gst_buffer = info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer")
+        return Gst.PadProbeReturn.OK
+
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        obj_counter = {PGIE_CLASS_ID_VEHICLE: 0, PGIE_CLASS_ID_PERSON: 0}
+        frame_number = frame_meta.frame_num
+
+        l_obj = frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                obj_meta = pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+            
+            obj_counter[obj_meta.class_id] += 1
+            obj_meta.rect_params.border_color.set(0.0, 0.0, 1.0, 0.8)  # Blue
+
+            try:
+                l_obj = l_obj.next
+            except StopIteration:
+                break
+
+        display_meta = pyds.nvds_acquire_display_meta_from_pool(batch_meta)
+        display_meta.num_labels = 1
+        py_nvosd_text_params = display_meta.text_params[0]
+        py_nvosd_text_params.display_text = "Frame={} Objects={} Vehicles={} People={}".format(
+            frame_number, sum(obj_counter.values()), obj_counter[PGIE_CLASS_ID_VEHICLE], obj_counter[PGIE_CLASS_ID_PERSON]
+        )
+        py_nvosd_text_params.x_offset = 10
+        py_nvosd_text_params.y_offset = 12
+        py_nvosd_text_params.font_params.font_name = "Serif"
+        py_nvosd_text_params.font_params.font_size = 10
+        py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
+        py_nvosd_text_params.set_bg_clr = 1
+        py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
+
+        print(pyds.get_string(py_nvosd_text_params.display_text))
+        pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+
+        try:
+            l_frame = l_frame.next
+        except StopIteration:
+            break
+
+    return Gst.PadProbeReturn.OK
+
+def main():
+    Gst.init(None)
+
+    pipeline = Gst.parse_launch(
+        "filesrc location=sample_720p.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! "
+        "nvstreammux name=mux batch-size=1 width=1280 height=720 ! "
+        "nvinfer config-file-path=pgie_config.txt ! "
+        "nvtracker ! "
+        "nvosd ! nvvideoconvert ! "
+        "nvv4l2h264enc bitrate=8000000 ! h264parse ! "
+        "qtmux ! filesink location=custom_code_output.mp4"
+    )
+
+    osd = pipeline.get_by_name("nvosd")
+    osd_sink_pad = osd.get_static_pad("sink")
+    osd_sink_pad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
+
+    loop = GLib.MainLoop()
+    pipeline.set_state(Gst.State.PLAYING)
+    
+    try:
+        loop.run()
+    except:
+        pass
+
+    pipeline.set_state(Gst.State.NULL)
+
+if __name__ == "__main__":
+    sys.exit(main())
