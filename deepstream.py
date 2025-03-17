20835d8b85cc67d19fca215e99278a723f45fba4
pratheesh-bi3 <pratheeshraj.prince@bi3technologies.com>
2025-03-17 16:04:24 +0530

    Update deepstream.py

--- deepstream.py
+++ deepstream.py
@@ -133,7 +133,7 @@ def main(args):
     nvtracker.set_property("tracker-width", 640)
     nvtracker.set_property("tracker-height", 480)
     nvtracker.set_property("gpu_id", 0)
-    nvtracker.set_property("ll-lib-file", "/opt/nvidia/deepstream/deepstream/lib/libnvds_nvdcf.so")
+    nvtracker.set_property("ll-lib-file", "/opt/nvidia/deepstream/deepstream/lib/libnvds_nvmultiobjecttracker.so")
     nvtracker.set_property("ll-config-file", "/opt/nvidia/deepstream/deepstream-7.1/deepstream_python_apps/deepstream_python_apps/apps/deepstream-test2/config_tracker_NvDCF_perf.yml")
     
     sink.set_property("location", "tracker_test_1.mp4")
